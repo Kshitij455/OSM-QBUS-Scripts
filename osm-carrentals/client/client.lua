4066cef6267e519648ad35ac7d829963d6a0cbf6
Kshitij455 <88702135+Kshitij455@users.noreply.github.com>
2021-10-10 01:57:57 +0530

    Update client.lua

--- osm-carrentals/client/client.lua
+++ osm-carrentals/client/client.lua
@@ -1,7 +1,7 @@
 -- ENTIRE SCRIPT MADE BY OSMIUM#0001 | DISCORD.IO/OSMFX --
 -- This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA. 
 
-QBCore = nil
+local QBCore = exports['qb-core']:GetCoreObject()
 
 Citizen.CreateThread(function() 
 	while true do
